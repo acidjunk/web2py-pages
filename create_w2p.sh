f546d9797c788b56e7f298e1c959519182f668de
Rene Dohmen <rene@formatics.nl>
2014-05-08 16:42:40 +0200

    Switched to appliance style to ease the workflow

--- create_w2p.sh
+++ create_w2p.sh
@@ -1,5 +1,6 @@
-tar -cvzf ../web2py.plugin.pages_with_languages.w2p views/ controllers/ modules/ models/ static/ languages/
-tar -cvzf ../web2py.plugin.pages.w2p views/ controllers/ modules/ models/ static/ 
+#TODO: write a python script for this
+tar -cvzf ../web2py.plugin.pages_with_languages.w2p views/page views/page_custom views/plugin_ckeditor views/plugin_tagging controllers/page* modules/plugin_ckeditor.py models/page*.py models/0_db.py models/plugin_ckeditor.py models/plugin_tagging.py static/page static/plugin_ckeditor static/plugin_markitup static/plugin_wiki languages/
+tar -cvzf ../web2py.plugin.pages.w2p views/page views/page_custom views/plugin_ckeditor views/plugin_tagging controllers/page* modules/plugin_ckeditor.py models/page*.py models/0_db.py models/plugin_ckeditor.py models/plugin_tagging.py static/page static/plugin_ckeditor static/plugin_markitup static/plugin_wiki
 echo "Shall I copy the w2p file to release location? y/n "
 read ans
 if [ ${ans} == 'y' ]; then
