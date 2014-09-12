84a2f571896973d7d609270687b4a44050cebf59
Rene Dohmen <acidjunk@gmail.com>
2014-09-12 14:54:28 +0200

    Added script to copy plugin to an existing app

--- copy_to_app.sh
+++ copy_to_app.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
-#Copy complete stuff to app
-echo "Do you really want to run this? it will delete the previous copy..."
-echo "It needs a reference web2py app to operate"
+# A script to copy the web2py-pages files to an existing app 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Run this script with an PATH to the application you want to patch. E.g.: ./copy_to_app.sh ../your_awesome_web2py_app "
+    exit 1
+fi
 
-EMPTY_REFERENCE_PATH=~/web2py/applications/page_ref_original
-TARGET_LOCATION=~/web2py/applications/page1_ref
+TARGET_LOCATION=$1
+echo "Using $1 as target location"
 
-echo "Anwer 'yes' to continue"
+echo "Are you sure you want to copy the files? You could also use the w2p plugin, this operation could screwup your app. Anwer 'yes' to continue"
 read ans
 if [ "$ans" != "yes" ]; then
     echo "Quitting"
-    exit
+    exit 1
 fi
 
-echo "Starting delete"
-rm -rf $TARGET_LOCATION
-echo "Copying reference app"
-cp -rv $EMPTY_REFERENCE_PATH $TARGET_LOCATION
-
-
-echo "Merging module in the new tree"
-cp -rv controllers $TARGET_LOCATION/
-cp -rv modules $TARGET_LOCATION/
-cp -rv models $TARGET_LOCATION/
-cp -rv views $TARGET_LOCATION/
-cp -rv static $TARGET_LOCATION/
+echo "Merging files in the new tree"
+# Controllers
+cp -v controllers/page*.py $TARGET_LOCATION/controllers/
+# Modules
+cp -v modules/plugin_ckeditor.py $TARGET_LOCATION/modules/
+# Models
+cp -v models/page*.py $TARGET_LOCATION/models/
+cp -v models/0_db.py $TARGET_LOCATION/models/
+cp -v models/plugin_ckeditor.py $TARGET_LOCATION/models/
+cp -v models/plugin_tagging.py $TARGET_LOCATION/models/
+# Views
+cp -rv views/page $TARGET_LOCATION/views/
+cp -rv views/page_custom $TARGET_LOCATION/views/
+cp -rv views/plugin_ckeditor $TARGET_LOCATION/views/
+cp -rv views/plugin_tagging $TARGET_LOCATION/views/
+#Static assets
+cp -rv static/page $TARGET_LOCATION/static/
+cp -rv static/plugin_ckeditor $TARGET_LOCATION/static/
+cp -rv static/plugin_markitup $TARGET_LOCATION/static/
+cp -rv static/plugin_wiki $TARGET_LOCATION/static/
